258d22297774eb72d9dd8149ff358d540aa1e406
Celena <celutaumos@gmail.com>
2025-05-13 15:29:42 -0300

    test progreso

--- index.js
+++ index.js
@@ -42,3 +42,81 @@ const frases = [
       mostrarFrase(frases[index]);
     }, 1000); 
   }, 10000); // cada 10 segundos cambia
+
+
+  /*test*/
+  const botonesClima = document.querySelectorAll(".opciones button");
+  const resultadoTexto = document.querySelector(".texto-resultado");
+  const resultadoFamilia = document.getElementById("resultadoFamilia");
+  const pregunta2 = document.getElementById("pregunta2");
+  const svgContainer = document.querySelector(".pocima-container");
+  const reiniciarBtn = document.getElementById("btnReiniciar");
+  
+  // Familia -> color y texto
+  const familias = {
+    acuatica: {
+      texto: "Tu familia olfativa es: Acuáticos/Verdes.",
+      color: "#66cccc"
+    },
+    frutal: {
+      texto: "Tu familia olfativa es: Frutales/Florales exóticas.",
+      color: "#ff6699"
+    },
+    madera: {
+      texto: "Tu familia olfativa es: Maderas/Especias.",
+      color: "#cc9966"
+    },
+    ambarada: {
+      texto: "Tu familia olfativa es: Gourmand/Ambarados.",
+      color: "#9966cc"
+    }
+  };
+  
+  // Carga SVG inline
+  async function cargarSVGInline(url) {
+    const response = await fetch(url);
+    const svgText = await response.text();
+    
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(svgText, "image/svg+xml");
+    const svg = doc.querySelector("svg");
+  
+    // Asegurar tamaño fijo por código
+    svg.setAttribute("width", "250");
+    svg.setAttribute("height", "250");
+  
+    svgContainer.innerHTML = ""; // Limpiar anterior
+    svgContainer.appendChild(svg);
+  }
+  
+  
+  botonesClima.forEach(btn => {
+    btn.addEventListener("click", async () => {
+      const tipo = btn.dataset.fragancia;
+      const familia = familias[tipo];
+      if (familia) {
+        resultadoTexto.textContent = familia.texto;
+        resultadoFamilia.classList.remove("hidden");
+        pregunta2.classList.add("hidden");
+  
+        // Cargar el SVG correcto
+        await cargarSVGInline(`./img/potion/potion04.svg`);
+  
+        // Cambiar el color del líquido por clase "relleno"
+        const relleno = svgContainer.querySelector(".relleno");
+        if (relleno) {
+          relleno.setAttribute("fill", familia.color);
+        }
+      }
+    });
+  });
+  
+  // Reiniciar prueba
+  reiniciarBtn.addEventListener("click", () => {
+    resultadoFamilia.classList.add("hidden");
+    pregunta2.classList.remove("hidden");
+    resultadoTexto.textContent = "";
+    cargarSVGInline(`./img/potion/potion00.svg`);
+  });
+  
+  
